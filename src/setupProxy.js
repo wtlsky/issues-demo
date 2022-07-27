0f60b7a96003876088c235a4a2bb1ca8a2555ae7
wutianliang <wutianliang@tanmarket.cn>
2022-07-27 19:26:43 +0800

    添加代理配置

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,13 +2,23 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware('/yiyan', {
-      target: 'https://v1.hitokoto.cn/',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/yiyan': ''
-      }
-    })
-  )
+
+  // 代理测试服务器
+  app.use(createProxyMiddleware('/proxy', {
+    target: 'https://blue.wechatsi.com/',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/proxy': ''
+    }
+  }))
+
+  // 代理一言组件
+  app.use(createProxyMiddleware('/yiyan', {
+    target: 'https://v1.hitokoto.cn/',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/yiyan': ''
+    }
+  }))
+
 }
