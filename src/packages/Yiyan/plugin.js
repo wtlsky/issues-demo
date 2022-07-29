d1d1bc59346d84a7bcc902260b827f9715b0ce7a
wutianliang <wutianliang@tanmarket.cn>
2022-07-29 14:16:53 +0800

    sync

--- src/packages/Yiyan/plugin.js
+++ src/packages/Yiyan/plugin.js
@@ -1,6 +1,4 @@
-import { BasePlugin } from 'amis-editor';
-import { Button, toast } from 'amis';
-import style from './scss/plugin.module.scss'
+import { BasePlugin, getSchemaTpl } from 'amis-editor';
 
 
 export default class YiyanPlugin extends BasePlugin {
@@ -25,11 +23,18 @@ export default class YiyanPlugin extends BasePlugin {
   // 拖入组件里面时的初始数据
   scaffold = {
     type: 'yi-yan',
-    link: '/yiyan/',
+    api: {
+      method: 'get',
+      url: '/yiyan/'
+    },
     label: '百度一下,你就知道',
     showAbout: true,
-    sentence: 'a'
-  };
+    sentence: 'a',
+    initFetch: false,
+    body: [
+      { type: 'container', body: ['内容区'] }
+    ]
+  }
 
   // 用来生成预览图的
   previewSchema = {
@@ -39,118 +44,113 @@ export default class YiyanPlugin extends BasePlugin {
   panelTitle = '一言';
   panelBodyCreator (context) {
     return [
-      {
-        type: 'tabs',
-        tabsMode: 'line',
-        className: 'm-t-n-xs',
-        contentClassName: 'no-border p-l-none p-r-none',
-        tabs: [
-          {
-            title: '常规',
-            controls: [
-              {
-                type: 'text',
-                name: 'link',
-                label: '接口地址',
-                description: '因一言官方接口存在跨域问题，需要后端代理，请在此填写接口地址'
-              },
-              {
-                type: 'switch',
-                name: 'showAbout',
-                label: '展示"关于一言"'
-              },
-              {
-                type: "radios",
-                label: "句子类型",
-                name: "sentence",
-                description: "这个选项用来演示自定义事件,会在切换类型时弹出一个Toast",
-                options: [
-                  {
-                    label: "动画",
-                    value: "a"
-                  },
-                  {
-                    label: "漫画",
-                    value: "b"
-                  },
-                  {
-                    label: "游戏",
-                    value: "c"
-                  },
-                  {
-                    label: "文学",
-                    value: "d"
-                  },
-                  {
-                    label: "原创",
-                    value: "e"
-                  },
-                  {
-                    label: "来自网络",
-                    value: "f"
-                  },
-                  {
-                    label: "其他",
-                    value: "g"
-                  },
-                  {
-                    label: "影视",
-                    value: "h"
-                  },
-                  {
-                    label: "诗词",
-                    value: "i"
-                  },
-                  {
-                    label: "网易云",
-                    value: "j"
-                  },
-                  {
-                    label: "哲学",
-                    value: "k"
-                  },
-                  {
-                    label: "抖机灵",
-                    value: "l"
-                  }
-                ],
-                onEvent: {
-                  change: {
-                    actions: [
-                      {
-                        actionType: "custom",
-                        script: this.handleSentenceChange.bind(this, context.id)
-                      }
-                    ]
-                  }
+      getSchemaTpl('tabs', [
+        {
+          title: '常规',
+          body: [
+            getSchemaTpl('switch', {
+              name: 'showAbout',
+              label: '展示"关于一言"'
+            }),
+
+            getSchemaTpl('radios', {
+              type: "radios",
+              label: "句子类型",
+              name: "sentence",
+              description: "这个选项用来演示自定义事件,会在切换类型时刷新一下编辑器里的组件实例内容",
+              options: [
+                {
+                  label: "动画",
+                  value: "a"
                 },
-                inline: false,
-                columnsCount: 2
+                {
+                  label: "漫画",
+                  value: "b"
+                },
+                {
+                  label: "游戏",
+                  value: "c"
+                },
+                {
+                  label: "文学",
+                  value: "d"
+                },
+                {
+                  label: "原创",
+                  value: "e"
+                },
+                {
+                  label: "来自网络",
+                  value: "f"
+                },
+                {
+                  label: "其他",
+                  value: "g"
+                },
+                {
+                  label: "影视",
+                  value: "h"
+                },
+                {
+                  label: "诗词",
+                  value: "i"
+                },
+                {
+                  label: "网易云",
+                  value: "j"
+                },
+                {
+                  label: "哲学",
+                  value: "k"
+                },
+                {
+                  label: "抖机灵",
+                  value: "l"
+                }
+              ],
+              onEvent: {
+                change: {
+                  actions: [
+                    {
+                      actionType: "custom",
+                      script: this.handleSentenceChange.bind(this, context.id)
+                    }
+                  ]
+                }
               },
-              {
-                type: 'container',
-                name: 'clickAction',
-                description: '这个按钮是用来演示如何在配置栏中调用组件内部方法',
-                children: () => (
-                  <Button className={style.configBtn} onClick={this.reloadText.bind(this, context.id)} level="primary">请求内容</Button>
-                )
-              }
-            ]
-          }
-        ]
-      }
+              inline: false,
+              columnsCount: 2
+            })
+          ]
+        },
+        {
+          title: '接口',
+          body: [
+            getSchemaTpl('switch', {
+              name: 'initFetch',
+              label: '初始是否拉取'
+            }),
+
+            getSchemaTpl('api', {
+              name: 'api',
+              label: '接口地址',
+              description: '因一言官方接口存在跨域问题，需要后端代理，请在此填写接口地址'
+            })
+          ]
+        },
+        {
+          title: '外观',
+          body: [getSchemaTpl('className')]
+        },
+      ])
     ];
   }
 
-  reloadText (id) {
+  handleSentenceChange (id) {
     const { manager } = this
     const { store } = manager
     const node = store.getNodeById(id);
     const component = node.getComponent()
     component.getYiyan() // renderer中定义的组件内部方法
-  }
-
-  handleSentenceChange (id) {
-    toast.success('这个选项用来演示自定义事件,会在切换类型时弹出一个Toast')
   }
 }
--- src/packages/Yiyan/renderer.js
+++ src/packages/Yiyan/renderer.js
@@ -10,12 +10,15 @@ export default class YiyanRenderer extends React.Component {
   }
 
   componentDidMount () {
-    this.getYiyan()
+    const { initFetch } = this.props
+    if (initFetch) {
+      this.getYiyan()
+    }
   }
 
   getYiyan () {
-    const { env, link, sentence } = this.props
-    env.fetcher({ url: link, data: { c: sentence } }).then(({ data }) => {
+    const { env, api, sentence } = this.props
+    env.fetcher(api, { c: sentence }).then(({ data }) => {
       this.setState({ data })
     })
   }
@@ -27,18 +30,20 @@ export default class YiyanRenderer extends React.Component {
   }
 
   render () {
-    const { theme, showAbout, id } = this.props
-    const { hitokoto, from, from_who, type } = this.state.data
+    const { className, showAbout, id } = this.props
+    const { hitokoto = '我变秃了，也变强了', from = '一拳超人', from_who = 'ONE', type } = this.state.data
     const about = () => (<a className='about' href="https://hitokoto.cn/about">关于一言</a>)
 
     return (
-      <div className={[style['yi-yan'], theme].join(' ')} style={{ backgroundImage: `url(https://source.unsplash.com/random/1920x200?x=${id})` }}>
-        <div className='container'>
-          <div className='content'>{hitokoto}</div>
-          <div className='from'>@{from}-{from_who || '佚名'}</div>
-          <div className='footer'>
-            <div className='reviewer'>{sentenceMap[type] || '动画'}</div>
-            {showAbout && about()}
+      <div className={[style['yi-yan'], className].join(' ')}>
+        <div className='card' style={{ backgroundImage: `url(https://source.unsplash.com/random/1920x200?x=${id})` }}>
+          <div className='container'>
+            <div className='content'>{hitokoto}</div>
+            <div className='from'>@{from}-{from_who || '佚名'}</div>
+            <div className='footer'>
+              <div className='reviewer'>{sentenceMap[type] || '动画'}</div>
+              {showAbout && about()}
+            </div>
           </div>
         </div>
         {this.renderBody()}
